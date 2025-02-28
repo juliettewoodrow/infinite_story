3639a4cc772a34d4be9e05339449dda3191c22e0
Chris Piech <piech@cs.stanford.edu>
2025-02-28 16:51:59 -0500

    refactor

---
+++ materials/infinitestory/infinite_story.py
@@ -0,0 +1,17 @@
+import json
+from notopenai import NotOpenAI
+import os
+from graphics import Canvas
+
+# follow the instructions in the notopenai handout to get your free api key
+CLIENT = NotOpenAI(api_key="your_api_key_here")
+STORY_NAME = "original_big"
+
+def main():
+    print("Infinite Story")
+    # TODO: your code here
+
+
+
+if __name__ == "__main__":
+    main()
